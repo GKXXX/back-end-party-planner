d691e8a28e9b8815a0ce1eeeba5c0577319eea15
GKXXX <57709564+GKXXX@users.noreply.github.com>
2024-06-21 15:12:18 +0200

    encoding password

--- src/main/java/com/example/party_planner/controller/AuthentificationController.java
+++ src/main/java/com/example/party_planner/controller/AuthentificationController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.beans.Encoder;
+import java.util.Base64;
+
 @Controller
 @RequestMapping("/auth")
 public class AuthentificationController {
@@ -22,11 +25,16 @@ public class AuthentificationController {
     @Autowired
     private UserMapper userMapper;
 
+
+    private Encoder encoder;
+
     @Autowired
     private JwtService jwtService;
 
     @PostMapping(value = "/register")
     public ResponseEntity<String> register(@RequestBody UserDto userDto) {
+        String encodedPassword = Base64.getEncoder().encodeToString(userDto.getPassword().getBytes());
+        userDto.setPassword(encodedPassword);
         userService.createUser(userDto);
         return ResponseEntity.ok().body("Done.");
     }
@@ -34,9 +42,9 @@ public class AuthentificationController {
     @PostMapping(value = "/connect")
     public ResponseEntity<String> login(@RequestBody AuthRequest authRequest) {
         UserDto user = userService.findUserByEmail(authRequest.username());
-        System.out.println(user.toString());
         if (user.getEmail() != null) {
-            if (user.getPassword().equals(authRequest.password())) {
+            String encodedPassword = Base64.getEncoder().encodeToString(authRequest.password().getBytes());
+            if (user.getPassword().equals(encodedPassword)) {
                 return ResponseEntity.ok().body(jwtService.generateToken(user));
             } else {
                 return ResponseEntity.ok().body("Authentication failed.");
